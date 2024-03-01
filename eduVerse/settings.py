17139afa8c65034ac3eeafe99800d55073c7d223
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-02 01:25:11 +0600

    customize Admin

--- eduVerse/settings.py
+++ eduVerse/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 STATIC_DIR =os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS =[
--- eduVerse/urls.py
+++ eduVerse/urls.py
@@ -1,12 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app_users.urls')),
     path('curriculum/',include('curriculum.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns() # new
+
 
 from django.conf import settings
 from django.conf.urls.static import static
